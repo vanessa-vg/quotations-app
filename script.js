1825268c90807a9f6a1f8bbc14222c17f79d11dd
Vanessa Valencia-Garcia <vanessa_vg@outlook.fr>
2025-03-01 14:27:31 +0100

    Etapes 5 et 6 finies

--- script.js
+++ script.js
@@ -2,19 +2,55 @@ const form = document.getElementById('form');
 const text = document.getElementById('user_quote');
 const author = document.getElementById('name');
 const submitButton = document.getElementById('submit-button');
+const quotesContainer = document.getElementById('quote-list');
+let counter = document.getElementById('count');
+let quoteCount = 0;
+
+function updateCounter() {
+    quoteCount++;
+
+    if (quoteCount <= 1) {
+        counter.textContent = `${quoteCount} citation`;
+    } else {
+        counter.textContent = `${quoteCount} citations`;
+    }
+};
+
+function addQuote(quote, author) {
+
+    const newQuote = document.createElement('p');
+    newQuote.classList.add('text');
+    newQuote.textContent = quote;
+
+    const newAuthor = document.createElement('p');
+    newAuthor.classList.add('author');
+    newAuthor.textContent = `- ${author}`;
+    
+   const newQuoteElement = document.createElement('div');
+    newQuoteElement.classList.add('quote');
+    newQuoteElement.appendChild(newQuote);
+    newQuoteElement.appendChild(newAuthor);
+
+    quotesContainer.appendChild(newQuoteElement);
+
+};
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log(text.value);
-    console.log(author.value);
-}
-);
+   
+    // Vérification des champs:
 
-function addQuote(quote, author) {
-    const newQuote = document.createElement('p');
-    newQuote.classList.add('text');
-    const newAuthor = document.createElement('p');
-    newAuthor.classList.add('author');
-    newQuote = text.value
-    newAuthor = author.value
-};
+    if (text.value.trim() === "" || author.value.trim() === "" ) {
+        alert('Veuillez remplir tous les champs !')
+        return;
+    }
+    addQuote(text.value, author.value);
+
+    updateCounter()
+
+
+    // Réititialiser les champs après soumission
+
+    text.value = "";
+    author.value = "";
+});
